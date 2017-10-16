ac6f3252d7ee1db858d55a5519cd0c18de530190
Yerlan <bayekeshov@gmail.com>
2017-10-15 21:12:32 -0400

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :portfolios
+  get '/home' => 'pages#home'
+
+  get '/about' => 'pages#about'
+
+  get '/contact' => 'pages#contact'
+
+  resources :blogs
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
